dba624c10f3d2c6fa0a2e77a700b9634ca60a26b
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-11 00:01:05 +0530

    Recursion Problem - Tylor Series

---
+++ Recursion ( using Taylor Series )/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static class TylorSeries
+    {
+        static double P = 1, F = 1;
+        public static double ePowx(int x, int n)
+        {
+            double s;
+            if (n == 0)
+                return 1;
+            s = ePowx(x, n - 1);
+            P *= x;
+            F *= n;
+            return s + P / F;
+        }
+    }
+    static void Main(string[] args)
+    {
+         double x = TylorSeries.ePowx(2, 10);
+        Console.WriteLine(x);
+    }
+}
