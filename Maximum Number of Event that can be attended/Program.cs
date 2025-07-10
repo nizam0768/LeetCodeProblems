f4df8c9d0cfdab01297b67d5baa0ed90cd55aaaf
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-10 23:02:32 +0530

    Add project files.

---
+++ Maximum Number of Event that can be attended/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    public static int MaxEvents(int[][] events)
+    {
+        Array.Sort(events, (a, b) => a[1].CompareTo(b[1]));                
+        var lastEndTime = -1;
+        var count = 0;
+        foreach (var e in events)
+        {
+            if (e[0] > lastEndTime)
+            {
+                count++;
+                lastEndTime = e[1];
+            }
+        }
+        return count;
+    }
+    static void Main(string[] args)
+    {
+        int[][] events = new int[][]
+        {
+            new int[] {1, 2},
+            new int[] {3, 4},
+            new int[] {0, 6},
+            new int[] {5, 7},
+            new int[] {8, 9},
+            new int[] {1, 2}
+        };
+        Console.WriteLine(MaxEvents(events)); // Output: 5
+    }
+}
