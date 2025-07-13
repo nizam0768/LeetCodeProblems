f2c4acaf91ccc2e072593e3289bc506504aeeffa
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-13 23:57:29 +0530

    added comment

--- Recursion ( using Taylor Series )/Program.cs
+++ Recursion ( using Taylor Series )/Program.cs
@@ -20,3 +20,8 @@
         Console.WriteLine(x);
     }
 }
+/*
+ * To find e^x using the recursive function, we need to use static variables. A function can return only one value, and when we need to include multiple values in a recursive function, we use static variables. The Taylor Series is a combination of multiple values like sum, power and factorial term, hence we will use static variables.
+
+For the power of x, we will use p, and for factorials, we will use f as static variables. 
+ */
