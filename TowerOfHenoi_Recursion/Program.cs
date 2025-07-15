e2bad617b88cd1b5e1ec8beeedf26119e7c766aa
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-15 21:38:38 +0530

    Add TowerOfHenoi_Recursion project and implementation
    This commit introduces a new project "TowerOfHenoi_Recursion" to the solution `LeetCodeProblems.sln`. It includes a `Program.cs` file that implements the Tower of Hanoi algorithm using recursion, with a `Main` method to initialize the process for 3 disks. Additionally, the project file `TowerOfHenoi_Recursion.csproj` is created, targeting .NET 8.0 and enabling implicit usings and nullable reference types.

---
+++ TowerOfHenoi_Recursion/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+     void TowerofHenoi(int n, int A, int B, int C)
+    {
+        if(n>0)
+        {
+            TowerofHenoi(n - 1, A, C, B);
+            Console.WriteLine($"({A},{C})");
+            TowerofHenoi(n - 1, B, A, C);
+        }
+    }
+    static void Main(string[] args)
+    {
+        int n = 3; 
+        Program program = new Program();
+        program.TowerofHenoi(n,1,2,3); // A, B and C are names of rods
+    }
+}
