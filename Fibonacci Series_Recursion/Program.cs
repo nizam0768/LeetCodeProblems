f892a895ac07d4b69320545aa6aa106563f7b753
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-16 23:57:41 +0530

    Add Fibonacci Series project and recursive implementation
    Created `Fibonacci Series_Recursion.csproj` with .NET properties.
    Added `Program` class with a recursive method to calculate Fibonacci numbers.
    Updated `LeetCodeProblems.sln` to include the new project and added build configurations for Debug and Release modes.

---
+++ Fibonacci Series_Recursion/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    int rfib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return rfib(n - 1) + rfib(n - 2);
+    }
+    static void Main(string[] args)
+    {
+        int n = 5; // Number of terms in the Fibonacci series
+        Program program = new Program();
+        int rfi = program.rfib(n);
+        Console.WriteLine(rfi);
+    }
+}
