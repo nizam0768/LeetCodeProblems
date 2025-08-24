a21ce444b8e33b59e06456f1a081aa8c518fcb4a
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-08-24 11:52:25 +0530

    Bubble Sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    public void BubbleSort(int[] A, int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            int flag = 0;  // reset before each pass
+
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (A[j] > A[j + 1])  // ascending order
+                {
+                    // swap A[j] and A[j+1]
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                    flag = 1;
+                }
+            }
+
+            if (flag == 0) // no swaps → already sorted
+                break;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+        int n = array.Length;
+
+        program.BubbleSort(array, n);
+
+        Console.WriteLine("Sorted array:");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
