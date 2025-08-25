a40df66a0d3bd2bb25c9b47823be33fcb9a33e83
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-08-25 22:27:11 +0530

    insertion sort sorting

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public void InsertionSort(int[] A, int n)
+    {
+        for(int i = 1; i<n; i++)
+        {
+            int key = A[i];
+            int j = i - 1;
+            while(j >=0 && A[j] > key)
+            {
+                A[j + 1] = A[j];
+                j--;
+            }
+            A[j + 1] = key;
+        }
+    }
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        int[] array = { 12, 11, 13, 5, 6 };
+        int n = array.Length;
+        program.InsertionSort(array, n);
+        Console.WriteLine("Sorted array:");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
